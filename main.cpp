fee6ae8a622461a6e91ab5294f34ba845b6a907a
HarutoTanaka-go <k024g0048@g.neec.ac.jp>
2025-10-27 12:36:50 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+
+typedef void(*PFunc)(int);
+
+// サイコロの出目を決定する為の関数
+int roll_dice() {
+	return rand() % 6;
+}
+
+
+// 判定を行うコールバック関数
+void judge_result(int result) {
+
+	int input;
+
+	printf("サイコロの出目が奇数(1)か偶数(2)か入力:");
+	scanf_s("%d", &input);
+
+	printf("判定中...\n");
+
+	Sleep(3000);
+
+	if ((result % 2 == 1 && input == 1) || (result % 2 == 0 && input == 2)) {
+		printf("正解\n");
+	}
+	else {
+		printf("不正解\n");
+	}
+
+	printf("サイコロの目は%d\n", result);
+}
+
+
+int main(void) {
+
+	srand((int)time(NULL));
+
+	int dice_result = roll_dice();
+
+	PFunc callback = judge_result;
+
+	callback(dice_result);
+
+
+	return 0;
+}
